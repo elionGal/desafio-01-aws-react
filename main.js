191d6184c3f24a75725d0aa2f64614aa7cb833a4
Elion <eliongalvao@gmail.com>
2024-09-06 15:51:00 -0300

    feat(funcionalidades): pequenas alterações de estilo e começo da implementação de funcionalidade

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
-//setTimeout(function() {
-//window.location.href = 'Star.html';
-// }, 4000);
+//setTimeout(function() 
+//{
+//window.location.href = 'Show1.html';
+//}, 4000);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backButton = document.getElementById('Vector');
+
+    if (backButton) {
+        const path = window.location.pathname;
+
+        let destination = '';
+
+        if (path.includes('Show2.html')) 
+        {
+            destination = 'Show1.html';
+        } else (path.includes('Show3.html')) 
+        {
+            destination = 'Show2.html'; 
+        } 
+
+        backButton.addEventListener('click', function() {
+            window.location.href = destination;
+        });
+    }0
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backButton = document.getElementById('NextButton');
+
+    if (backButton) {
+        const path = window.location.pathname;
+
+        let destination = '';
+
+        if (path.includes('Show1.html')) 
+        {
+            destination = 'Show2.html';
+        } 
+        else 
+        if (path.includes('Show2.html')) 
+        {
+            destination = 'Show3.html'; 
+        }
+        else
+        if (path.includes('Show3.html'))
+        {
+            destination = 'Login.html'; 
+        } 
+
+        backButton.addEventListener('click', function() {
+            window.location.href = destination;
+        });
+    }0
+});
